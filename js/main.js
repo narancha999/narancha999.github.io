437751e70adf60af29747e4986315a787643d839
Takahito nara <takahito.n.works@gmail.com>
2019-08-09 00:08:55 +0900

    reserve：modal add

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
+////__ reserve：空欄のフォーム色変え
 $(function () {
   const formInput = $('.js-form-input');
   $(formInput).change(function () {
-    console.log('changeされたよ');
+    // console.log('changeされたよ');
     let formValu = $(this).val();
     if (formValu) {
       $(this).css({ 'background-color': '#ffffff' });
@@ -13,3 +14,13 @@ $(function () {
   });
 });
 
+////__ reserve：モーダルウィンドウ処理
+$(function () {
+  //「席を決定する」ボタン押下されたら、
+  //選択されている席Noを取得し、フォームに値をセット
+  $('#seat_submit').click(function () {
+    let seatValue = $('#select_seat').val();
+    $('#seat').val(seatValue).css({ 'background-color': '#ffffff' });
+    $('#myModal').modal('hide');
+  });
+});
